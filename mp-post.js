910ff6d6a3f7f0bec0a0e9d1597db74462e96fff
Peter Kosyh <p.kosyh@gmail.com>
2019-04-13 09:37:01 +0300

    up

--- mp-post.js
+++ mp-post.js
@@ -7,6 +7,10 @@ FS.mount(MEMFS,{},'/games');
 
 var parser_start, parser_stop, parser_cmd, parser_restart, parser_load, parser_autoload, parser_path, parser_clear
 
+function startsWith(str, word) {
+	return str.lastIndexOf(word, 0) === 0;
+}
+
 Module['postRun'].push(function() {
 	console.log("Starting...");
 	parser_start = Module.cwrap('parser_start', 'number', ['string'])
@@ -29,12 +33,16 @@ Module['postRun'].push(function() {
 			url = metatags[mt].getAttribute("content");
 		}
 	}
-
+	var crossio = "https://cors-anywhere.herokuapp.com/";
 	if (!url && typeof window === "object") {
 		argv = window.location.search.substr(1).trim().split('&');
 		if (!argv[0])
 			argv = [];
-		url = argv[0];
+		if (startsWith(argv[0], "http:") || startsWith(argv[0], "https:")) {
+			url = crossio + argv[0];
+		} else {
+			url = argv[0];
+		}
 	}
 
 	if (!url)
