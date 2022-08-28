4de1c17835e5978b73b40d39a03c693aa04aa918
Peter Kosyh <pkosyh@yandex.ru>
2022-08-28 17:25:47 +0300

    CI fix/7

--- build.sh
+++ build.sh
@@ -57,4 +57,4 @@ test -d release/lib || mkdir release/lib
 cp -f metaparser-wasm.* metaparser-js.* release/ && rm -f release/*.html && cp -f index.html release/
 cp -rf lib/* release/lib && cp -f README release/ && cp -f ChangeLog release/
 
-cp -f release/metaparser-wasm.* release/metaparser-js.* site/games/ && cp -f release/index.html site/games && cp -rf release/lib/* site/games/lib/
+# cp -f release/metaparser-wasm.* release/metaparser-js.* site/games/ && cp -f release/index.html site/games && cp -rf release/lib/* site/games/lib/
